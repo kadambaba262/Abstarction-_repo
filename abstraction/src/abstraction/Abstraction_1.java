c0a58d14c23347d7f08f798e4c693f7c051e02dd
Babasaheb <kadambaba262@gmail.com>
2023-01-25 13:37:22 +0530

    "Abstraction Commit"

---
+++ abstraction/src/abstraction/Abstraction_1.java
@@ -0,0 +1,14 @@
+package abstraction;
+
+public abstract class Abstraction_1 {
+	
+ public abstract void m1();
+ 
+ public abstract void m2();
+ 
+ public void m3() {
+	 System.out.println("complete method");
+ }
+
+
+}
